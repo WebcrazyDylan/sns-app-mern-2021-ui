574485b385a6b6dca229624c9761feefbb97974d
WebcrazyDylan <webcrazy@naver.com>
2021-10-22 14:17:16 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# SNS React UI - Frontend
+
+Create social media project using React functional components and React Hooks.
+
+- Source Code    :  https://github.com/WebcrazyDylan/sns-react-ui-2021
+- Demo Website :  N/A
+
+- Dylan Park : https://jonghyun.cf
---
+++ src/components/feed/Feed.js
@@ -0,0 +1,17 @@
+import Share from "../share/Share";
+import Post from "../post/Post";
+import { Posts } from "../../dummyData";
+import "./feed.css";
+
+export default function Feed() {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
+}
